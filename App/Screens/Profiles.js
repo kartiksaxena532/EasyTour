72c022f9b0e66b2ed2ac08ad529793ce994fa4d2
Vaanya Sharma <vaanyasharma.vn@gmail.com>
2023-09-23 16:33:54 +0530

    change 1

--- App/Screens/Profiles.js
+++ App/Screens/Profiles.js
@@ -1,4 +1,4 @@
-import { View, Text,Dimensions, StatusBar} from 'react-native'
+/*import { View, Text,Dimensions, StatusBar} from 'react-native'
 import React from 'react'
 import tw from 'twrnc';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -6,18 +6,39 @@ import Colors from '../Shared/Colors';
 export default function Profiles() {
   return (
 <View className="flex bg-amber-500">
-   
-    <LinearGradient
-        colors={[Colors.WHITE, "transparent"]}
-        style={{ padding: 20, width: Dimensions.get("screen").width }}
-      ><Text>hello</Text>
-        </LinearGradient>
+  
     <View style={{paddingTop:30,paddingLeft:20}}>
     <Text style={{fontSize:30, color:'orange'}}>Profile</Text>
     </View>
 
-   <Text style={tw`text-5`}>Welcome ,<Text style={tw`text-10`}>Kartik Saxena</Text></Text>   
+   <Text style={tw`text-5`}>Welcome ,<Text style={tw`text-10`}>User</Text></Text>  
+   function App(){
+    return (
+      <div>
+        <login/>
+      </div>
+    );
+   } 
+  </View>
+  );
+}*/
+import React from 'react';
+import { View, Text } from 'react-native'; // Import from 'react-native'
+import tw from 'tailwind-rn'; // Assuming you are using 'tailwind-rn' for styling
+import { LinearGradient } from 'expo-linear-gradient';
+import Colors from '../Shared/Colors';
 
-   </View>
-  )
+export default function Profiles() {
+  return (
+    <View style={tw`flex bg-amber-500`}>
+      <View style={tw`pt-12 pl-4`}>
+        <Text style={{ fontSize: 30, color: 'orange' }}>Profile</Text>
+      </View>
+
+      <Text style={tw`text-5`}>Welcome,</Text>
+      <Text style={tw`text-10`}>User</Text>
+
+      {/* Removed the 'function App' section; it seems unrelated to this component */}
+    </View>
+  );
 }
